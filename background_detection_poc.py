51af3b6a41f40c95ed1e70d9863b56ac0ad237bd
Karol Działowski <karlososhd@gmail.com>
2019-02-10 14:41:07 +0100

    OpticalFlow with basic feature points

---
+++ background_detection_poc.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture('dynamic_sample.mp4')
+fgbg = cv.createBackgroundSubtractorMOG2()
+#fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+while(1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+    
+    # gray = cv.cvtColor(fgmask,cv.COLOR_GRAY2BGR)
+    # edges = cv.Canny(gray,50,150,apertureSize = 3)
+    #
+    # lines = cv.HoughLines(fgmask,500,np.pi/180,200)
+    # for line in lines:
+    #     for rho,theta in line:
+    #         a = np.cos(theta)
+    #         b = np.sin(theta)
+    #         x0 = a*rho
+    #         y0 = b*rho
+    #         x1 = int(x0 + 1000*(-b))
+    #         y1 = int(y0 + 1000*(a))
+    #         x2 = int(x0 - 1000*(-b))
+    #         y2 = int(y0 - 1000*(a))
+    #
+    #         cv.line(gray,(x1,y1),(x2,y2),(0,0,255),2)
+
+    cv.imshow('frame',fgmask)
+    #cv.imshow('frame', gray)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+cap.release()
+cv.destroyAllWindows()
---
+++ line_detection_poc.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2 as cv2
+
+cap = cv2.VideoCapture('dynamic_sample.mp4')
+while(1):
+    ret, frame = cap.read()
+
+    # load gray image and add gaussian blur
+    #img = cv2.imread('src.png')
+    img = frame
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    kernel_size = 5
+    blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+
+    # edge detection
+    low_threshold = 10
+    high_threshold = 100
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+    # houghlines to get the lines
+    rho = 1  # distance resolution in pixels of the Hough grid
+    theta = np.pi / 180  # angular resolution in radians of the Hough grid
+    threshold = 100  # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 20  # minimum number of pixels making up a line
+    max_line_gap = 50  # maximum gap in pixels between connectable line segments
+    line_image = np.copy(img) * 0  # creating a blank to draw lines on
+
+    # Run Hough on edge detected image
+    # Output "lines" is an array containing endpoints of detected line segments
+    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                        min_line_length, max_line_gap)
+
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+
+    # Draw the lines on the  image
+    lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
+
+    cv2.imshow('frame', lines_edges)
+    #cv2.imshow('frame', edges)
+    #cv.imshow('frame', gray)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
