6a5bed4606633d53c345602f79962df9ff6209ff
Karol Dzialowski <karlososhd@gmail.com>
2019-08-16 21:01:45 +0200

    Select points of interest of pitch
    After clicking s stop pause video and add circle when left click
    
    Ref #2

--- pitch_map.py
+++ pitch_map.py
@@ -12,9 +12,14 @@ import cv2
 
 class PitchMap:
     def __init__(self, tracking_method=None):
+        """
+        :param tracking_method: if left default (None) then there's no tracking and detection
+        is performed in every frame
+        """
         self.__video_name = 'Dynamic_Barca_Real.mp4'
+        self.__window_name = f'PitchMap: {self.__video_name}'
         self.__fl = frame_loader.FrameLoader(self.__video_name)
-        self.__display = frame_display.Display(f'PitchMap: {self.__video_name}')
+        self.__display = frame_display.Display(self.__window_name)
 
         self.__trackers = cv2.MultiTracker_create()
         self.__frame_number = 0
@@ -26,33 +31,50 @@ class PitchMap:
         }
         self.__tracking_method = tracking_method
 
+        cv2.namedWindow(self.__window_name)
+        cv2.setMouseCallback(self.__window_name, self.add_point)
+
+        self.__pause = False
+        self.__out_frame = None
+
     @staticmethod
-    def exit_user_input():
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
+    def input_point(key):
+        if key == 115:  # s key
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def input_exit(key):
+        if key == 27:
             return True
         else:
             return False
 
     def loop(self):
         while True:
-            frame = self.__fl.load_frame()
-            frame = imutils.resize(frame, width=600)
+            if not self.__pause:
+                frame = self.__fl.load_frame()
+                frame = imutils.resize(frame, width=600)
 
-            grass_mask = mask.grass(frame)
-            edges = detect.edges_detection(grass_mask)
-            lines_frame = detect.lines_detection(edges, grass_mask)
+                grass_mask = mask.grass(frame)
+                edges = detect.edges_detection(grass_mask)
+                lines_frame = detect.lines_detection(edges, grass_mask)
 
-            if self.__tracking_method is not None:
-                self.tracking(grass_mask)
-            else:
-                bounding_boxes_frame, bounding_boxes, labels = detect.players_detection(grass_mask)
+                if self.__tracking_method is not None:
+                    self.tracking(grass_mask)
+                else:
+                    bounding_boxes_frame, bounding_boxes, labels = detect.players_detection(grass_mask)
 
-            out_frame = cv2.addWeighted(grass_mask, 0.8, lines_frame, 1, 0)
+                self.__out_frame = cv2.addWeighted(grass_mask, 0.8, lines_frame, 1, 0)
 
-            self.__display.show(out_frame)
-            if self.exit_user_input():
+            self.__display.show(self.__out_frame)
+
+            key = cv2.waitKey(1) & 0xff
+            if self.input_exit(key):
                 break
+            elif self.input_point(key):
+                self.__pause = not self.__pause
 
             self.__frame_number += 1
 
@@ -60,6 +82,11 @@ class PitchMap:
         self.__fl.release()
 
     def tracking(self, frame):
+        """
+        Update tracking and draw white boxes around tracking objects
+
+        :param frame:
+        """
         if self.__frame_number % 15 == 0:
             self.add_tracking_points(frame.copy())
 
@@ -70,6 +97,11 @@ class PitchMap:
             cv2.rectangle(frame, (x, y), (w, h), (255, 255, 255), 2)
 
     def add_tracking_points(self, frame):
+        """
+        Add points for tracking from detector
+        :param frame:
+        """
+
         bounding_boxes_frame, bounding_boxes, labels = detect.players_detection(frame)
         self.__trackers = cv2.MultiTracker_create()
         for i, label in enumerate(labels):
@@ -77,7 +109,16 @@ class PitchMap:
                 tracker = self.OPENCV_OBJECT_TRACKERS[self.__tracking_method]()
                 self.__trackers.add(tracker, frame, tuple(bounding_boxes[i]))
 
+    def add_point(self, event, x, y, flags, params):
+        """
+        Mouse callback. For adding points of interest for perspective transformation.
+        """
+
+        if self.__pause:
+            if event == cv2.EVENT_LBUTTONUP:
+                cv2.circle(self.__out_frame, (x, y), 5, (0, 255, 0), 2)
+
 
 if __name__ == '__main__':
-    pm = PitchMap(tracking_method='kcf')
+    pm = PitchMap()
     pm.loop()
