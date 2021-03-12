03472f4d45dc3cbe3a867d5dbc5883dd2ccedd12
Karol Dzialowski <karlososhd@gmail.com>
2021-03-12 21:11:43 +0100

    Generate points for advanced homography metric

--- comparison/mse_homographies_interpolation.py
+++ comparison/mse_homographies_interpolation.py
@@ -13,6 +13,32 @@ def calulate_iou(model_warp, model_warp_pred):
     return iou_score
 
 
+def generate_points_on_image(image):
+    # Generate points on warp image
+    x = np.linspace(0, image.shape[1]-1, 20)
+    y = np.linspace(0, image.shape[0]-1, 20)
+    X, Y = np.meshgrid(x, y)
+    points = np.column_stack([X.ravel(), Y.ravel()]).astype(int)
+
+    return points
+
+
+def transform_points(points, homo, inverse):
+    points = np.float32(points)
+    warped_positions = []
+    for circle in points.tolist():
+        circle = np.array(circle)
+        if circle.size == 2:
+            circle = np.append(circle, 1.)
+        if inverse:
+            circle_warped = np.linalg.inv(homo).dot(circle)
+        else:
+            circle_warped = homo.dot(circle)
+        circle_warped = circle_warped / circle_warped[2]
+        warped_positions.append(circle_warped)
+    return warped_positions
+
+
 def main():
     # Loading files
     input_file = "baltyk_starogard_1.mp4"
@@ -50,21 +76,33 @@ def main():
         model_warp = cv2.warpPerspective(pitch_model, np.linalg.inv(homo), frame_shape)
         model_warp_pred = cv2.warpPerspective(pitch_model, np.linalg.inv(homo_pred), frame_shape)
 
-        # Calculate IoU
-        iou_score = calulate_iou(model_warp, model_warp_pred)
-        iou_scores.append(iou_score)
-        print("IoU score: ", iou_score)
+        points = generate_points_on_image(warp)
+        # Filter points
+        points = [(x, y) for (x, y) in points if not np.array_equal(warp[y, x], [0, 0, 0])]
+        # Draw points
+        for (x, y) in (points):
+            cv2.circle(warp, (x, y), 2, (0, 0, 255), -1)
+
+        # Transform points to frame (multiplying by inverse homography)
+        frame_points = transform_points(points, homo, inverse=True)
+        # Draw on frame
+        for (x, y, _) in frame_points:
+            cv2.circle(frame, (int(x), int(y)), 2, (255, 0, 0), -1)
+
+        pred_points = transform_points(frame_points, homo_pred, inverse=False)
+        for (x, y, _) in pred_points:
+            cv2.circle(warp_pred, (int(x), int(y)), 2, (255, 0, 0), -1)
 
         # Visualisation
-        # cv2.imshow('orig', frame)
-        # cv2.imshow('warp', warp)
-        # cv2.imshow('warp_pred', warp_pred)
+        cv2.imshow('orig', frame)
+        cv2.imshow('warp', warp)
+        cv2.imshow('warp_pred', warp_pred)
         # cv2.imshow('model_warp', model_warp)
         # cv2.imshow('model_warp_pred', model_warp_pred)
         #
-        # k = cv2.waitKey(0)
-        # if k == 27:
-        #     break
+        k = cv2.waitKey(0)
+        if k == 27:
+            break
 
     print("Average IOU for video sequence:", np.mean(iou_scores))
 
